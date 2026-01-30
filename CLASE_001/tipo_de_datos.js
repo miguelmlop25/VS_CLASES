25b54108a4cec9e6d1b7813caaeb00e5694f64e6
Miguel Monroy <miguelmonroy2003lop@gmail.com>
2026-01-29 18:44:08 -0600

    Subiendo carpetas de clases

---
+++ CLASE_001/tipo_de_datos.js
@@ -0,0 +1,24 @@
+// tIPOS DATOS
+
+// STRINGS
+// Entre comillas
+"Hola soy un string" /* Tambien se puede comentar asi en JS */
+'Yo también soy un string' // ASI NO
+// MEJOR ASÍ
+
+// NUMBERS
+2
+2.1
+-1
+
+// BOOLEANS
+true
+false
+
+// NULL
+null
+
+// UNDEFINED
+undefined
+
+console.log(typeof true)
